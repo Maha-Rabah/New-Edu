23005e42ffd766a1ddeb62fa0f651d3ebbdc9129
EhabSe <ehab.sierwan@gmail.com>
2024-11-02 00:58:20 +0300

    flipping 180 contribute cards

--- src/modules/main/components/contribution/contribution.jsx
+++ src/modules/main/components/contribution/contribution.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './contributionStyle.css'
 import yellow from '../../../../assets/yellow.svg'
+import { FaRotate } from 'react-icons/fa6'
 
 
 const Contribution = ({conticard}) => {
+  const [flippedCards, setFlippedCards] = useState({});
+
+  const handleFlip = (id) => {
+    setFlippedCards((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id] // Toggle the flipped state for the clicked card only
+      
+    }));
+    console.log('flipped card', );
+  };
+
   return (
     <div className='contribution'>
         <h2 className='title'>Our Contribution to SDGs</h2>
-         <div className="container">
-      {conticard.map(function (item) {
+         <div className="contribution-container">
+          {
+            conticard.map((item) => (
+              <div className={`contri-card ${flippedCards[item.id] ? 'flipped' : ''}`} onClick={() => handleFlip(item.id)}>
+                <div className="card-front">
+                  <FaRotate className='rotate' />
+                  <img src={item.img} alt="" />
+                </div>
+                <div className="card-back">
+                  <img src={item.sdg} className='sdg-img' alt="" />
+                  <h3 className='sdg-title'>{item.title}</h3>
+                  <p className='sdg-text'>{item.text}</p>
+                </div>
+              </div>
+            ))
+            
+          }
+          </div>
+      {/* {conticard.map(function (item) {
                 return (
                   <>
                     <div className="contricard">
@@ -16,9 +45,12 @@ const Contribution = ({conticard}) => {
                     </div>
                   </>
                 );
-            })}
-      <img src={yellow} alt="" className='yellow1'/>
-      </div>
+            })} */}
+
+
+
+      {/* <img src={yellow} alt="" className='yellow1'/> */}
+      {/* </div> */}
     </div>
   )
 }
